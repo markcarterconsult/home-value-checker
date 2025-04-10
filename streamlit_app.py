e33fa75df5df77bfa6d680f5844d6d8eabb9b3a4
markcarterconsult <mark@musicrecordshop.com>
2025-04-10 16:24:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+st.set_page_config(page_title="🏡 Home Value Checker", layout="centered")
+
+st.title("🏡 What's Your Home Worth?")
+st.write("Fill out the form below and our AI will estimate your home's value.")
